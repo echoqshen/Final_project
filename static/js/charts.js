d83b8d7f05bdbf8274ffe6ced05f75473e8c833d
victoriaguille <91439219+victoriaguille@users.noreply.github.com>
2022-03-09 22:09:46 -0500

    updated dashboard code again

--- static/js/charts.js
+++ static/js/charts.js
@@ -31,3 +31,27 @@ function optionChanged(newDate) {
   buildCharts(newDate);
     
 }
+
+
+// Demographics Panel 
+function buildMetadata(date) {
+  d3.json("gender.json").then((data) => {
+    var gender = data.gender;
+    // Filter the data for the object with the desired date
+    var resultArray = gender.filter(sampleObj => sampleObj.DATE == dates);
+    var result = resultArray[0];
+    // Use d3 to select the panel with id of `#sample-metadata`
+    var PANEL = d3.select("#sample-metadata");
+
+    // Use `.html("") to clear any existing metadata
+    PANEL.html("");
+
+    // Use `Object.entries` to add each key and value pair to the panel
+    // Hint: Inside the loop, you will need to use d3 to append new
+    // tags for each key-value in the metadata.
+    Object.entries(result).forEach(([key, value]) => {
+      PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    });
+
+  });
+}
